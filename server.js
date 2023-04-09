5d85307bb5b906e5d55ef5612450530ca862e0ea
Anthony Stanton <anthonystanton314@gmail.com>
2023-04-08 23:19:58 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const constellations = [
+    {
+        name:'Orion',
+        meaning: 'The Hunter',
+        distance: '1.34 light years',
+    },
+    {
+        name:'Ursa Major',
+        meaning: 'Great Bear',
+        distance: '3.33 light years',
+    },
+    {
+        name:'Leo',
+        Meaning: 'The Lion',
+        distance: 'true'
+    }
+];
+
+module.exports = constellations;
